100808a42214ad77617d2ed2d6a11096b60344ec
Johan Rask <johan.rask@sonymobile.com>
2016-06-14 09:39:24 +0200

    Added license to files

--- lumbermill-core/src/main/java/lumbermill/RetryStrategy.java
+++ lumbermill-core/src/main/java/lumbermill/RetryStrategy.java
@@ -16,7 +16,20 @@ import java.util.Map;
  * <br>
  * http://blog.danlew.net/2016/01/25/rxjavas-repeatwhen-and-retrywhen-explained/
  */
-
+/*
+ * Copyright 2016 Sony Mobile Communications, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 public interface RetryStrategy {
 
 
